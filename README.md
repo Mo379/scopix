e40b1d3b285a25595e4357b218c12d01e7f8671e
Mustafa Omar <mustafa12211@hotmail.co.uk>
2025-09-29 09:48:18 +0100

    minor broken unfinished

---
+++ README.md
@@ -0,0 +1,17 @@
+# Coursely
+Solving the porblem of learning
+
+[KEY, FIX, BUG, CLEANUP, FEATURE, GENERALISATION, BACKEND, FRONTEND]
+## Key Development Milestones
+- [ ] KEY: Everything is super simple ?
+
+# Features and Optimisations
+- [ ] FEATURE: 
+
+
+## Bugs, Cleanup, Patches and Changes
+- [ ] FEATURE: 
+
+
+
+## Completed Tasks
--- main.py
+++ main.py
@@ -10,23 +10,23 @@ import logging as std_logger
 
 if __name__ == "__main__":
     root = Path('./')
+    configs = load_configs(root)
+
     std_logger.basicConfig(
         level=std_logger.INFO,
         format='%(asctime)s - %(levelname)s - %(message)s',
         stream=sys.stdout
     )
-    configs = load_configs(root)
-    train_set, eval_set, test_set = load_dataset(configs['dataset'])
     logger = load_logger(configs['logger'])
-    model_constructor = load_model(configs['model'])
-    dummy_net_constructor = DummyNetConstructor(
-        **configs['model']['DummyNet'],
-        Logger=logger
-    )
-    _, model_apply, model_param = dummy_net_constructor.construct()
 
+    train_set, eval_set, test_set = load_dataset(configs['dataset'])
     inputs, outputs, test_input, test_output = next(iter(eval_set))
-
+    std_logger.info(
+        "train, eval, test lengths: %d, %d, %d",
+        len(train_set),
+        len(eval_set),
+        len(test_set)
+    )
     std_logger.info(
         "input, ouput, test_input, test_output shapes: %s, %s, %s, %s",
         str(inputs.shape),
@@ -34,12 +34,14 @@ if __name__ == "__main__":
         str(test_input.shape),
         str(test_output.shape),
     )
-    std_logger.info(
-        "train, eval, test lengths: %d, %d, %d",
-        len(train_set),
-        len(eval_set),
-        len(test_set)
+
+    model_constructor = load_model(configs['model'])
+    dummy_net_constructor = DummyNetConstructor(
+        **configs['model']['DummyNet'],
+        Logger=logger
     )
+    _, model_apply, model_param = dummy_net_constructor.construct()
+
 
     for batch_index, (inputs, outputs, test_input, test_output) in enumerate(iter(test_set)):
         print(batch_index)
